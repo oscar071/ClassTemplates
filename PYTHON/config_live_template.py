089e32fac9204fb20cfb585b6b169a78c7adc399
oscar071 <78671011+oscar071@users.noreply.github.com>
2022-05-23 00:15:52 -0500

    Update config_live_template.py

--- PYTHON/config_live_template.py
+++ PYTHON/config_live_template.py
@@ -1,3 +1,15 @@
+# ________________________________Instructions_________________________________
+"""
+                    VARIABLES
+DAY:        , example -> 01
+MONTH:      , example -> jan
+YEAR:       , example -> 2021
+CREDITS:    , example -> AvantZen
+
+                    PROCESS:
+01.- Check all the class and delete this instructions
+"""
+# ________________________________Instructions_________________________________
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
@@ -8,14 +20,15 @@ __status__ = "Developing"
 
 __version__ = "1.0"
 
-__date__ = "feb/09/2021"
+__date__ = "//"
 
 __author__ = "Oscar Martinez"
 
-__credits__ = "AvantZen"
+__credits__ = ""
 
 
-from Config.app import App
+from config.app import App
+
 
 def run_config():
     App._name = 'NameApp'
