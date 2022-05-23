875fe9aa4fc5bea689ff76669b022ed5e6983f67
oscar071 <78671011+oscar071@users.noreply.github.com>
2022-05-23 00:13:56 -0500

    Update app_live_template.py

--- PYTHON/app_live_template.py
+++ PYTHON/app_live_template.py
@@ -1,3 +1,15 @@
+# ________________________________Instructions_________________________________
+"""
+                    VARIABLES
+DAY:        , example -> 01
+MONTH:      , example -> jan
+YEAR:       , example -> 2021
+CREDITS:    , example -> AvantZen
+
+                    PROCESS:
+01.- Check all the class and delete this instructions
+"""
+# ________________________________Instructions_________________________________
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
@@ -8,11 +20,11 @@ __status__ = "Developing"
 
 __version__ = "1.0"
 
-__date__ = "feb/09/2021"
+__date__ = "//"
 
 __author__ = "Oscar Martinez"
 
-__credits__ = "AvantZen"
+__credits__ = ""
 
 
 class App:
